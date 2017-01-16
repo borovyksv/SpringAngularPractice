49fc76a24a5d3be7025f64cac7c171a4d7dccb86
relzet <relzet69@gmail.com>
2017-01-17 00:36:44 +0200

    Java Config improved

---
+++ src/main/java/com/borovyksv/SpringAngularJS/javaconfig/SpringSecurityInit.java
@@ -0,0 +1,9 @@
+package com.borovyksv.SpringAngularJS.javaconfig;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by user-pc on 17.01.2017.
+ */
+public class SpringSecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
