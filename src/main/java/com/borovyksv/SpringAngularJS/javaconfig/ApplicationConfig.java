bcd47b55333d29b1373ba600dbbd74fac79586d6
relzet <relzet69@gmail.com>
2017-01-19 19:10:17 +0200

    AngularJS testing

--- src/main/java/com/borovyksv/SpringAngularJS/javaconfig/ApplicationConfig.java
+++ src/main/java/com/borovyksv/SpringAngularJS/javaconfig/ApplicationConfig.java
@@ -4,11 +4,8 @@ package com.borovyksv.SpringAngularJS.javaconfig;
  * Created by user-pc on 17.01.2017.
  */
 
-import com.borovyksv.SpringAngularJS.mvc.quarts.CronQuartzTask;
-import com.borovyksv.SpringAngularJS.mvc.quarts.QuartzTask;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -22,11 +19,6 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
-import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 import org.springframework.ui.velocity.VelocityEngineFactoryBean;
 import org.springframework.web.client.RestTemplate;
 
@@ -39,8 +31,8 @@ import java.util.Properties;
  */
 @Configuration
 @PropertySource(value = "classpath:util.properties") //<context:property-placeholder location=".." />
-@ComponentScan(basePackages = "com.borovyksv.SpringAngularJS.mvc.scheduling")
-@EnableScheduling //task:annotation-driven
+//@ComponentScan(basePackages = "com.borovyksv.SpringAngularJS.mvc.scheduling")
+//@EnableScheduling //task:annotation-driven
 public class ApplicationConfig {
 
     /**
@@ -177,79 +169,79 @@ public class ApplicationConfig {
         return jpa;
     }
 
-   /* Quartz scheduling configuration */
-
-    /**
-     *   <bean id="simpleTrigger" class="org.springframework.scheduling.quartz.SimpleTriggerFactoryBean">
-     */
-    @Bean(name = "simpleTrigger")
-    public SimpleTriggerFactoryBean getSimpleTriggerFactoryBean() {
-        SimpleTriggerFactoryBean stfb = new SimpleTriggerFactoryBean();
-
-        stfb.setJobDetail(getSimpleQuartzJob().getObject());
-
-        stfb.setRepeatInterval(1000);
-        stfb.setStartDelay(1000);
-        return stfb;
-    }
-
-    /**
-     *  <bean id="cronTrigger" class="org.springframework.scheduling.quartz.CronTriggerFactoryBean">
-     */
-    @Bean(name = "cronTrigger")
-    public CronTriggerFactoryBean getCronTriggerFactoryBean() {
-        CronTriggerFactoryBean ctfb = new CronTriggerFactoryBean();
-
-        ctfb.setJobDetail(getQuartzCronJob().getObject());
-
-        ctfb.setCronExpression("0/30 * * * * ?");
-        return ctfb;
-    }
-
-    /**
-     *  <bean id="simpleQuartzJob" class="org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean">
-     */
-    @Bean(name = "simpleQuartzJob")
-    public MethodInvokingJobDetailFactoryBean getSimpleQuartzJob() {
-        MethodInvokingJobDetailFactoryBean factoryBean = new MethodInvokingJobDetailFactoryBean();
-        factoryBean.setTargetBeanName("simpleQuartzTask");
-        factoryBean.setTargetMethod("simpleTaskMethod");
-        return factoryBean;
-    }
-    /**
-     *  <bean id="quartzCronJob" class="org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean">
-     */
-    @Bean(name = "quartzCronJob")
-    public MethodInvokingJobDetailFactoryBean getQuartzCronJob() {
-        MethodInvokingJobDetailFactoryBean factoryBean = new MethodInvokingJobDetailFactoryBean();
-        factoryBean.setTargetBeanName("cronQuartzTask");
-        factoryBean.setTargetMethod("cronTaskMethod");
-        return factoryBean;
-    }
-
-    /**
-     *  <bean id="simpleQuartzTask" class="ru.javastudy.mvcHtml5Angular.mvc.quartz.QuartzTask" />
-     */
-    @Bean(name = "simpleQuartzTask")
-    public QuartzTask getSimpleQuartzTask() {
-        return new QuartzTask();
-    }
-
-    /**
-     *   <bean id="cronQuartzTask" class="ru.javastudy.mvcHtml5Angular.mvc.quartz.CronQuartzTask" />
-     */
-    @Bean(name = "cronQuartzTask")
-    public CronQuartzTask getCronQuartzTask() {
-        return new CronQuartzTask();
-    }
-
-    /**
-     *  <bean class="org.springframework.scheduling.quartz.SchedulerFactoryBean">
-     */
-    @Bean(name = "schedulerFactoryBean")
-    public SchedulerFactoryBean getSchedulerFactoryBean() {
-        SchedulerFactoryBean scheduler  = new SchedulerFactoryBean();
-        scheduler.setTriggers(getSimpleTriggerFactoryBean().getObject(), getCronTriggerFactoryBean().getObject());
-        return scheduler ;
-    }
+//   /* Quartz scheduling configuration */
+//
+//    /**
+//     *   <bean id="simpleTrigger" class="org.springframework.scheduling.quartz.SimpleTriggerFactoryBean">
+//     */
+//    @Bean(name = "simpleTrigger")
+//    public SimpleTriggerFactoryBean getSimpleTriggerFactoryBean() {
+//        SimpleTriggerFactoryBean stfb = new SimpleTriggerFactoryBean();
+//
+//        stfb.setJobDetail(getSimpleQuartzJob().getObject());
+//
+//        stfb.setRepeatInterval(1000);
+//        stfb.setStartDelay(1000);
+//        return stfb;
+//    }
+//
+//    /**
+//     *  <bean id="cronTrigger" class="org.springframework.scheduling.quartz.CronTriggerFactoryBean">
+//     */
+//    @Bean(name = "cronTrigger")
+//    public CronTriggerFactoryBean getCronTriggerFactoryBean() {
+//        CronTriggerFactoryBean ctfb = new CronTriggerFactoryBean();
+//
+//        ctfb.setJobDetail(getQuartzCronJob().getObject());
+//
+//        ctfb.setCronExpression("0/30 * * * * ?");
+//        return ctfb;
+//    }
+//
+//    /**
+//     *  <bean id="simpleQuartzJob" class="org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean">
+//     */
+//    @Bean(name = "simpleQuartzJob")
+//    public MethodInvokingJobDetailFactoryBean getSimpleQuartzJob() {
+//        MethodInvokingJobDetailFactoryBean factoryBean = new MethodInvokingJobDetailFactoryBean();
+//        factoryBean.setTargetBeanName("simpleQuartzTask");
+//        factoryBean.setTargetMethod("simpleTaskMethod");
+//        return factoryBean;
+//    }
+//    /**
+//     *  <bean id="quartzCronJob" class="org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean">
+//     */
+//    @Bean(name = "quartzCronJob")
+//    public MethodInvokingJobDetailFactoryBean getQuartzCronJob() {
+//        MethodInvokingJobDetailFactoryBean factoryBean = new MethodInvokingJobDetailFactoryBean();
+//        factoryBean.setTargetBeanName("cronQuartzTask");
+//        factoryBean.setTargetMethod("cronTaskMethod");
+//        return factoryBean;
+//    }
+//
+//    /**
+//     *  <bean id="simpleQuartzTask" class="ru.javastudy.mvcHtml5Angular.mvc.quartz.QuartzTask" />
+//     */
+//    @Bean(name = "simpleQuartzTask")
+//    public QuartzTask getSimpleQuartzTask() {
+//        return new QuartzTask();
+//    }
+//
+//    /**
+//     *   <bean id="cronQuartzTask" class="ru.javastudy.mvcHtml5Angular.mvc.quartz.CronQuartzTask" />
+//     */
+//    @Bean(name = "cronQuartzTask")
+//    public CronQuartzTask getCronQuartzTask() {
+//        return new CronQuartzTask();
+//    }
+//
+//    /**
+//     *  <bean class="org.springframework.scheduling.quartz.SchedulerFactoryBean">
+//     */
+//    @Bean(name = "schedulerFactoryBean")
+//    public SchedulerFactoryBean getSchedulerFactoryBean() {
+//        SchedulerFactoryBean scheduler  = new SchedulerFactoryBean();
+//        scheduler.setTriggers(getSimpleTriggerFactoryBean().getObject(), getCronTriggerFactoryBean().getObject());
+//        return scheduler ;
+//    }
 }
